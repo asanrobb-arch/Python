9a61e31535bd5f1366fdc7462ebd14ab4566bccf
Ángela <asanrobb@ieszaidinvergeles.org>
2025-09-30 13:55:00 +0200

    Hacer ejercicio 4

---
+++ ejercicio4.py
@@ -0,0 +1,9 @@
+"""
+Ejercicio 4
+Escribir un programa que muestre por pantalla el resultado de
+la siguiente operación aritmética. ((3+2)/(2*5))²
+"""
+
+Operacion = ((3+2)/(2*5))**2
+
+print(f"El resultado de la operación aritmética ((3+2)/(2*5))² es: {Operacion}")

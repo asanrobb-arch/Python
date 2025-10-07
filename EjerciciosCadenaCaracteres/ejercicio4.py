e5bb002eb354c810031e81b0b736d5303bb3f93e
Ángela <asanrobb@ieszaidinvergeles.org>
2025-10-07 13:53:31 +0200

    Hacer ejercicio 4
    He añadido más secciones del teléfono.

---
+++ EjerciciosCadenaCaracteres/ejercicio4.py
@@ -0,0 +1,12 @@
+"""
+Ejercicio 4
+Los teléfonos de una empresa tienen el siguiente formato
+prefijo-número-extension donde el prefijo es el código del país +34, y la
+extensión tiene dos dígitos (por ejemplo +34-913724710-56). Escribir un
+programa que pregunte por un número de teléfono con este formato y muestre por
+pantalla el número de teléfono sin el prefijo y la extensión.
+"""
+
+telefono = str(input("Introduzca su teléfono incluyendo prefijo y extensión: "))
+
+print (f"El teléfono sin el prefijo: {telefono[0:4]} y la extensión: {telefono[13:16]} es = {telefono[4:13]}")

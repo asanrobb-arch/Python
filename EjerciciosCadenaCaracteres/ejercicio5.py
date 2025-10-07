0fd196667e2d3a102cff071ec5f1a0baa4924274
Ángela <asanrobb@ieszaidinvergeles.org>
2025-10-07 13:57:39 +0200

    Hacer ejercicio 5

---
+++ EjerciciosCadenaCaracteres/ejercicio5.py
@@ -0,0 +1,9 @@
+"""
+Ejercicio 5
+Escribir un programa que pida al usuario que introduzca una frase en la consola y
+muestre por pantalla la frase invertida.
+"""
+
+frase = str(input("Introduzca una frase: "))
+
+print (f"La frase invertida es: {frase[::-1]}")

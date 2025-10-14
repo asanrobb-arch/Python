0e09d0e2b647256db339bc422dc1dba8568b341f
Ángela <asanrobb@ieszaidinvergeles.org>
2025-10-14 13:23:00 +0200

    Hacer Ejercicio 2

---
+++ TANDA 3 - Ejercicios de Condicionales/ejercicio2.py
@@ -0,0 +1,17 @@
+"""
+Ejercicio 2
+Escribir un programa que almacene la cadena de caracteres contraseña en una
+variable, pregunte al usuario por la contraseña e imprima por pantalla si la
+contraseña introducida por el usuario coincide con la guardada en la variable sin
+tener en cuenta mayúsculas y minúsculas.
+"""
+
+contraseña = "contraseña"
+
+introducir_contraseña = str(input("Introduzca la contraseña: "))
+introducir_contraseña = introducir_contraseña.lower()
+
+if contraseña == introducir_contraseña:
+    print ("Contraseña correcta.")
+else:
+    print ("Contraseña incorrecta.")

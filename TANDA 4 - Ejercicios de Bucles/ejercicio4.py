e893d4ed2326d22a661bf3baa080175cac78ba36
Ángela <asanrobb@ieszaidinvergeles.org>
2025-10-21 14:38:33 +0200

    Hacer ejercicio 4

---
+++ TANDA 4 - Ejercicios de Bucles/ejercicio4.py
@@ -0,0 +1,13 @@
+"""
+Ejercicio 4
+Escribir un programa que pida al usuario un número entero positivo y muestre por
+pantalla la cuenta atrás desde ese número hasta cero separados por comas.
+"""
+
+numero = int(input("Introduzca un número entero positivo: "))
+
+while numero > 0:
+    print (numero, ", ")
+    numero -= 1
+
+print (0)

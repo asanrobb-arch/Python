41ad637d7c0b9597cd12e5b1f737845cdd19380a
Ángela <asanrobb@ieszaidinvergeles.org>
2025-12-09 13:32:06 +0100

    Hacer ejercicio 1

---
+++ TANDA 6 - Ejercicios de diccionarios/ejercicio1.py
@@ -0,0 +1,16 @@
+"""
+Ejercicio 1
+Escribir un programa que guarde en una variable el diccionario {'Euro':'€',
+'Dollar':'$', 'Yen':'¥'}, pregunte al usuario por una divisa y muestre su
+símbolo o un mensaje de aviso si la divisa no está en el diccionario.
+"""
+
+diccionario = {'Euro':'€', 'Dollar':'$', 'Yen':'¥'}
+
+divisa = input("Introduce una divisa: ")
+divisa = divisa.title()
+
+if divisa in diccionario:
+    print(f"La divisa {diccionario[divisa]} está en el diccionario.")
+else:
+    print("La divisa no está en el diccionario.")
